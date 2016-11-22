ca8085a17686e51f3f9d2f2ab34799bfc3451373
Vesa Kivistö <kivisto.vesku@gmail.com>
2016-11-22 23:04:38 +0200

    Add an example program with unit tests

---
+++ UnitTests/UnitTests/Basket.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace UnitTests
+{
+    public class Basket
+    {
+        public string Customer { get; set; }
+        public List<string> Contents { get; set; }
+        public double Price { get; set; }
+
+        public Basket(string customer, List<string> contents, double price)
+        {
+            this.Customer = customer;
+            this.Contents = contents;
+            this.Price = price;
+        }
+
+        public string ReturnCustomer()
+        {
+            return this.Customer;
+        }
+
+        public List<string> ReturnContents()
+        {
+            return this.Contents;
+        }
+
+        public double ReturnPrice()
+        {
+            return this.Price;
+        }
+
+        public void AddProduct(string product, double productPrice)
+        {
+            this.Contents.Add(product);
+            this.Price += productPrice;
+        }
+
+        public void DeleteProduct(string product, double productPrice)
+        {
+            foreach (string item in this.Contents)
+            {
+                if (item == product)
+                {
+                    this.Contents.Remove(item);
+                    this.Price -= productPrice;
+                }
+            }
+        }
+
+        public double CalculateDiscountPrice(double percent)
+        {
+            double discount = percent / 100;
+            return this.Price - this.Price * discount;
+        }
+
+        public double CalculateAverageItemPrice()
+        {
+            return this.Price / this.Contents.Count;
+        }
+    }
+}
---
+++ UnitTests/UnitTests/TestClass.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        List<string> items;
+        Basket basket;
+        [SetUp]
+        public void SetUp()
+        {
+            items = new List<string>();
+            items.Add("omena");
+            items.Add("banaani");
+            basket = new Basket("Jari", items, 25.0);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            items = null;
+            basket = null;
+        }
+
+        [Test]
+        public void IsCustomerString()
+        {
+            Assert.That(basket.ReturnCustomer(), Is.InstanceOf(typeof(string)));
+        }
+
+        [Test]
+        public void IsPriceDouble()
+        {
+            Assert.That(basket.ReturnPrice(), Is.InstanceOf(typeof(double)));
+        }
+
+        [Test]
+        public void IsContentsList()
+        {
+            Assert.That(basket.ReturnContents(), Is.InstanceOf(typeof(List<string>)));
+        }
+
+        [Test]
+        public void AddToContents()
+        {
+            basket.AddProduct("kala", 5.0);
+            Assert.That(basket.ReturnContents(), Contains.Item("kala"));
+        }
+
+        [Test]
+        public void DeleteFromList()
+        {
+            basket.DeleteProduct("kala", 5.0);
+            Assert.That(basket.ReturnContents(), !Contains.Item("kala"));
+        }
+
+        [Test]
+        public void CalculateDiscount()
+        {
+            Assert.That(basket.CalculateDiscountPrice(10), Is.EqualTo(22.5));
+        }
+
+        [Test]
+        public void CalculateAveragePrice()
+        {
+            Assert.That(basket.CalculateAverageItemPrice(), Is.EqualTo(12.5));
+        }
+    }
+}
